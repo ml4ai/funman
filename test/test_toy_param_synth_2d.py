32709b6d002161e361db5cd5ee52825a408ebbb3
Drisana Mosaphir <dmosaphir@dmosaphir.local>
2022-09-14 16:36:17 -0500

    add 2d test case

--- test/test_toy_param_synth_2d.py
+++ test/test_toy_param_synth_2d.py
@@ -1,6 +1,6 @@
+from pysmt.shortcuts import get_model, And, Symbol, FunctionType, Function, Equals, Int, Real, substitute, TRUE, FALSE, Iff, Plus, ForAll, LT, simplify, GT, LE, GE
 import sys
 sys.path.append('/Users/dmosaphir/SIFT/Projects/ASKEM/code/funman/src')
-from pysmt.shortcuts import get_model, And, Symbol, FunctionType, Function, Equals, Int, Real, substitute, TRUE, FALSE, Iff, Plus, ForAll, LT, simplify, GT, LE, GE
 from pysmt.typing import INT, REAL, BOOL
 import unittest
 import os
@@ -10,13 +10,16 @@ class TestCompilation(unittest.TestCase):
     def test_toy(self):
         
         x = Symbol("x", REAL)
-        parameters = [Parameter(x)]
+        y = Symbol("y", REAL)
+        parameters = [Parameter(x), Parameter(y)]
 
-        # 0.0 <= x <= 5
+        # 0.0 < x < 5.0, 10.0 < y < 12.0
         model = Model(
             And(
                 LE(x, Real(5.0)), 
-                GE(x, Real(0.0))
+                GE(x, Real(0.0)),
+                LE(y, Real(12.0)), 
+                GE(y, Real(10.0))
                 )
             )
         
