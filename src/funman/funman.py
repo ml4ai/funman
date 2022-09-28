dd9f1d507606e95ab124f5cd08a2ca883c297929
Daniel Bryce <danbryce410@gmail.com>
2022-09-28 12:22:44 -0500

    parallel plotting fixes

--- src/funman/funman.py
+++ src/funman/funman.py
@@ -1,3 +1,5 @@
+from asyncio.subprocess import Process
+from multiprocessing import Queue
 from funman.constants import BIG_NUMBER, POS_INFINITY, NEG_INFINITY
 from funman.scenario import ParameterSynthesisScenario
 from funman.scenario import (
@@ -5,7 +7,7 @@ from funman.scenario import (
     AnalysisScenarioResult,
     ParameterSynthesisScenarioResult,
 )
-from funman.search import Box, BoxSearch
+from funman.search import Box, BoxSearch, SearchConfig
 
 
 import logging
@@ -20,16 +22,23 @@ class Funman(object):
             ParameterSynthesisScenario: self.synthesize_parameters
         }
 
-    def solve(self, problem: AnalysisScenario) -> AnalysisScenarioResult:
-        return self.scenario_handlers[type(problem)](problem)
+    def solve(
+        self, problem: AnalysisScenario, config: SearchConfig = SearchConfig()
+    ) -> AnalysisScenarioResult:
+        return self.scenario_handlers[type(problem)](problem, config=config)
 
     def synthesize_parameters(
-        self, problem: ParameterSynthesisScenario, tolerance: float = 0.1
+        self, problem: ParameterSynthesisScenario, config: SearchConfig = SearchConfig()
     ) -> ParameterSynthesisScenarioResult:
         """ """
 
         search = BoxSearch()
-        result = search.search(problem)
+        result = search.search(problem, config=config)
+
+        
+        result.plot(
+            title=f"Residual = {result.statistics.current_residual}, |boxes|={result.statistics.num_unknown}",
+        )
 
         # FIXME the parameter space will be added the to object below, in addition to the problem.
         #       Initially, the parameter space can be a set of boxes
--- src/funman/search.py
+++ src/funman/search.py
@@ -1,5 +1,9 @@
+import ctypes
+from curses.ascii import EM
 from functools import total_ordering
 from inspect import Parameter
+import os
+
 from typing import Dict, List, Union
 import time
 
@@ -12,7 +16,7 @@ from matplotlib.lines import Line2D
 import numpy as np
 from IPython.display import clear_output
 from datetime import datetime
-from multiprocessing import Pool, cpu_count, Queue, Process, active_children
+from multiprocessing import cpu_count, Queue, Process, Value, Array
 from queue import Empty
 
 import logging
@@ -179,19 +183,25 @@ class Box(object):
 
 class SearchStatistics(object):
     def __init__(self):
-        self.num_true = 0
-        self.num_false = 0
-        self.num_unknown = 0
-        self.residual = []
-        self.last_time = None
-        self.iteration_time = []
-        self.iteration_operation = []
+        self.num_true = Value("i", 0)
+        self.num_false = Value("i", 0)
+        self.num_unknown = Value("i", 0)
+        self.residuals = Queue()
+        self.current_residual = Value("d", 0.0)
+        self.last_time = Array(ctypes.c_wchar, "")
+        self.iteration_time = Queue()
+        self.iteration_operation = Queue()
+
+    def close(self):
+        self.residuals.close()
+        self.iteration_time.close()
+        self.iteration_operation.close()
 
 
 class SearchConfig(object):
     def __init__(self, *args, **kwargs) -> None:
         self.tolerance = kwargs["tolerance"] if "tolerance" in kwargs else 1e-1
-        self.queue_timeout = 1
+        self.queue_timeout = kwargs["queue_timeout"] if "queue_timeout" in kwargs else 1
 
 
 class SearchEpisode(object):
@@ -205,60 +215,103 @@ class BoxSearchEpisode(SearchEpisode):
     ) -> None:
         super(BoxSearchEpisode, self).__init__()
         self.unknown_boxes = Queue()  # queue.PriorityQueue()
-        self.true_boxes = Queue()
-        self.false_boxes = Queue()
+        # self.true_boxes = Queue()
+        # self.false_boxes = Queue()
+        self.true_boxes = []
+        self.false_boxes = []
+
         self.config = config
         self.problem = problem
         self.num_parameters = len(self.problem.parameters)
-        self.iteration = 0
+        self.iteration = Value("i", 0)
+        self.internal_process_id = 0
+
+        self.closed = False
+        # self.t_boxes = None
+        # self.f_boxes = None
+        # self.u_boxes = None
 
     def initial_box(self) -> Box:
         return Box(self.problem.parameters)
 
     def on_start(self):
-        self.statistics.last_time = datetime.now()
+        self.statistics.last_time.value = str(datetime.now())
+
+    def close(self):
+        self.unknown_boxes.close()
+        # self.true_boxes.close()
+        # self.false_boxes.close()
+        self.statistics.close()
+
+    # def read_queues(self):
+    #     # self.true_boxes.put_nowait(None)
+    #     # self.true_boxes.close()
+    #     self.t_boxes = []
+    #     try:
+    #         while True:
+    #             self.t_boxes.append(self.true_boxes.get_nowait())
+    #     except Empty:
+    #         pass
+    #     # self.false_boxes.put_nowait(None)
+    #     # self.false_boxes.close()
+    #     self.f_boxes = []
+    #     try:
+    #         while True:
+    #             self.f_boxes.append(self.false_boxes.get_nowait())
+    #     except Empty:
+    #         pass
+
+    # # self.unknown_boxes.put_nowait(None)
+    # # # self.unknown_boxes.close()
+    # # self.u_boxes = [b for b in iter(self.unknown_boxes.get, None)]
+    # self.close()
+    # self.closed = True
 
     def on_iteration(self):
-        l.debug(f"false boxes: {self.false_boxes}")
-        l.debug(f"true boxes: {self.true_boxes}")
-        l.debug(f"unknown boxes: {self.unknown_boxes}")
-        # if self.iteration % 50 == 0:
-        #     self.plot(
-        #         self.true_boxes,
-        #         self.false_boxes,
-        #         self.unknown_boxes,
-        #         title=f"Residual = {self.statistics.residual[-1]}, |boxes|={self.statistics.num_unknown}",
-        #     )
-        self.iteration += 1
+        if self.internal_process_id == 1 and self.iteration.value % 10 == 0:
+            self.plot(
+                # self.true_boxes,
+                # self.false_boxes,
+                # self.unknown_boxes,
+                title=f"Residual = {self.statistics.current_residual}, |boxes|={self.statistics.num_unknown}",
+            )
+            pass
+        self.iteration.value = self.iteration.value + 1
 
     def add_unknown(self, box: Union[Box, List[Box]]):
         if isinstance(box, list):
             for b in box:
                 if b.width() > self.config.tolerance:
                     self.unknown_boxes.put(b, timeout=self.config.queue_timeout)
-                    self.statistics.num_unknown += 1
+                    self.statistics.num_unknown.value += 1
         else:
             if box.width() > self.config.tolerance:
                 self.unknown_boxes.put(box)
-                self.statistics.num_unknown += 1
+                with self.statistics.num_unknown.get_lock():
+                    self.statistics.num_unknown.value += 1
 
     def add_false(self, box: Box):
-        self.false_boxes.put_nowait(box)
-        self.statistics.num_false += 1
-        self.statistics.iteration_operation.append("f")
+        # self.false_boxes.put(box)
+        self.false_boxes.append(box)
+        with self.statistics.num_false.get_lock():
+            self.statistics.num_false.value += 1
+        self.statistics.iteration_operation.put("f")
 
     def add_true(self, box: Box):
-        self.true_boxes.put_nowait(box)
-        self.statistics.num_true += 1
-        self.statistics.iteration_operation.append("t")
+        # self.true_boxes.put(box)
+        self.true_boxes.append(box)
+        with self.statistics.num_true.get_lock():
+            self.statistics.num_true.value += 1
+        self.statistics.iteration_operation.put("t")
 
     def get_unknown(self):
         box = self.unknown_boxes.get(timeout=self.config.queue_timeout)
-        self.statistics.num_unknown -= 1
-        self.statistics.residual.append(box.width())
+        self.statistics.num_unknown.value = self.statistics.num_unknown.value - 1
+        self.statistics.current_residual.value = box.width()
+        self.statistics.residuals.put(box.width())
         this_time = datetime.now()
-        self.statistics.iteration_time.append(this_time - self.statistics.last_time)
-        self.statistics.last_time = this_time
+        # FIXME self.statistics.iteration_time.put(this_time - self.statistics.last_time.value)
+        # FIXME self.statistics.last_time[:] = str(this_time)
         return box
 
     def plotBox(self, interval: Interval = Interval(-20, 20)):
@@ -269,9 +322,6 @@ class BoxSearchEpisode(SearchEpisode):
 
     def plot(
         self,
-        true_boxes,
-        false_boxes,
-        unknown_boxes,
         plot_bounds: Box = None,
         title="Feasible Regions",
     ):
@@ -281,9 +331,9 @@ class BoxSearchEpisode(SearchEpisode):
         clear_output(wait=True)
 
         if self.num_parameters == 1:
-            self.plot1D(true_boxes, false_boxes, unknown_boxes, plot_bounds)
+            self.plot1D(plot_bounds=plot_bounds)
         elif self.num_parameters == 2:
-            self.plot2D(true_boxes, false_boxes, unknown_boxes, plot_bounds)
+            self.plot2D(plot_bounds=plot_bounds)
         else:
             raise Exception(
                 f"Plotting for {self.num_parameters} >= 2 is not supported."
@@ -307,8 +357,19 @@ class BoxSearchEpisode(SearchEpisode):
         x = np.linspace(x_limits.lb, x_limits.ub, 1000)
         plt.fill_between(x, y_limits.lb, y_limits.ub, color=color)
 
-    def plot1D(self, true_boxes, false_boxes, unknown_boxes, plot_bounds):
+    def plot1D(
+        self,
+        true_boxes: Queue = None,
+        false_boxes: Queue = None,
+        unknown_boxes: Queue = None,
+        plot_bounds: Box = None,
+    ):
         p1 = self.problem.parameters[0]
+
+        true_boxes = true_boxes if true_boxes else self.true_boxes
+        false_boxes = false_boxes if false_boxes else self.false_boxes
+        unknown_boxes = unknown_boxes if unknown_boxes else self.unknown_boxes
+
         clear_output(wait=True)
         for i in iter(true_boxes.get, None):
             if plot_bounds.intersects(i) and i.finite():
@@ -318,19 +379,122 @@ class BoxSearchEpisode(SearchEpisode):
             if plot_bounds.intersects(i) and i.finite():
                 self.plot1DBox(i, p1, color="r")
         plt.xlabel(p1.name)
-
         plt.xlim([plot_bounds.bounds[p1].lb, plot_bounds.bounds[p1].ub])
 
-    def plot2D(self, true_boxes, false_boxes, unknown_boxes, plot_bounds):
+    def _get_box_queue(self, q: Queue, q_size: int):
+        result = []
+        # q.put(None)
+        try:
+            # while q_size > 0 and not q.empty():
+            while not q.empty():
+                # q_size -= 1
+                val = q.get_nowait()
+                # if val:
+                result.append(val)
+                # else:
+                # break
+        except Empty:
+            pass
+
+        for b in result:
+            q.put_nowait(b)
+
+        return result
+
+    def plot2D(
+        self,
+        true_boxes: Queue = None,
+        false_boxes: Queue = None,
+        unknown_boxes: Queue = None,
+        plot_bounds: Box = None,
+    ):
         p1 = self.problem.parameters[0]
         p2 = self.problem.parameters[1]
 
-        for i in list(false_boxes.queue):
-            if plot_bounds.intersects(i) and i.finite():
-                self.plot2DBox(i, p1, p2, color="r")
-        for i in list(true_boxes.queue):
-            if plot_bounds.intersects(i) and i.finite():
-                self.plot2DBox(i, p1, p2, color="g")
+        true_boxes = true_boxes if true_boxes else self.true_boxes
+        false_boxes = false_boxes if false_boxes else self.false_boxes
+        unknown_boxes = unknown_boxes if unknown_boxes else self.unknown_boxes
+
+        if self.closed:
+            t_boxes = self.t_boxes
+            f_boxes = self.f_boxes
+        else:
+            # with self.statistics.num_true.get_lock():
+            #     n_true = self.statistics.num_true.value
+            # t_boxes = self._get_box_queue(true_boxes, n_true)
+
+            # with self.statistics.num_false.get_lock():
+            #     n_false = self.statistics.num_false.value
+            # f_boxes = self._get_box_queue(false_boxes, n_false)
+            t_boxes = true_boxes
+            f_boxes = false_boxes
+
+        for b in t_boxes:
+            if b and plot_bounds.intersects(b) and b.finite():
+                self.plot2DBox(b, p1, p2, color="g")
+        for b in f_boxes:
+            if b and plot_bounds.intersects(b) and b.finite():
+                self.plot2DBox(b, p1, p2, color="r")
+
+        # tmp = Queue()
+        # false_boxes.put(None)
+        # j = 0
+        # print("Begin Plotting.")
+        # try:
+        #     while not false_boxes.empty():
+        #         # for i in iter(false_boxes.get_nowait, None):
+        #         # print("Getting...")
+        #         i = false_boxes.get(block=False, timeout=self.config.queue_timeout)
+        #         # print(f"Got {i}")
+        #         if i:
+        #             # print("Putting...")
+        #             tmp.put_nowait(i)
+        #             # print(f"Put {i}")
+        #         j += 1
+        #         if plot_bounds.intersects(i) and i.finite():
+        #             self.plot2DBox(i, p1, p2, color="r")
+        # except Empty:
+        #     pass
+        # # print(f"Moved {j} boxes from false to tmp.")
+        # # tmp.put_nowait(None)
+        # j = 0
+        # try:
+        #     # for i in iter(tmp.get_nowait, None):
+        #     while not tmp.empty():
+        #         i = tmp.get_nowait()
+        #         if i:
+        #             false_boxes.put_nowait(i)
+        #         j += 1
+        # except Empty:
+        #     pass
+        # print(f"Moved {j} boxes from tmp to false.")
+        # # # true_boxes.put(None)
+        # j = 0
+        # try:
+        #     # for i in iter(true_boxes.get_nowait, None):
+        #     while not true_boxes.empty():
+        #         i = true_boxes.get_nowait()
+        #         if i:
+        #             tmp.put_nowait(i)
+        #         j += 1
+        #         if plot_bounds.intersects(i) and i.finite():
+        #             self.plot2DBox(i, p1, p2, color="g")
+        # except Empty:
+        #     pass
+        # print(f"Moved {j} boxes from true to tmp.")
+        # # tmp.put(None)
+        # j = 0
+        # try:
+        #     # for i in iter(tmp.get_nowait, None):
+        #     while not tmp.empty():
+        #         i = tmp.get_nowait()
+        #         if i:
+        #             true_boxes.put_nowait(i)
+        #         j += 1
+        # except Empty:
+        #     pass
+        # print(f"Moved {j} boxes from tmp to true.")
+
         plt.xlabel(p1.name)
         plt.ylabel(p2.name)
         plt.xlim([plot_bounds.bounds[p1].lb, plot_bounds.bounds[p1].ub])
@@ -345,9 +509,12 @@ class BoxSearch(object):
     def split(self, box: Box, episode: BoxSearchEpisode):
         b1, b2 = box.split()
         episode.add_unknown([b1, b2])
-        episode.statistics.iteration_operation.append("s")
+        episode.statistics.iteration_operation.put("s")
 
     def expand(self, rval: Queue, episode: BoxSearchEpisode):
+        if episode.internal_process_id == 0:
+            episode.add_unknown(episode.initial_box())
+
         while True:
             try:
                 box = episode.get_unknown()
@@ -374,7 +541,8 @@ class BoxSearch(object):
                     # box does not intersect f, so it is in t (true region)
                     episode.add_true(box)
                 episode.on_iteration()
-        rval.put(episode.iteration)
+        episode.close()
+        rval.put({"true_boxes": episode.true_boxes, "false_boxes": episode.false_boxes})
         # return True
 
     def search(
@@ -385,9 +553,9 @@ class BoxSearch(object):
         self.episodes.append(episode)
         episode.on_start()
 
-        episode.add_unknown(episode.initial_box())
-        number_of_processes = cpu_count()
+        number_of_processes = 2  # cpu_count()
         processes = []
+
         rval = Queue()
 
         # creating processes
@@ -401,8 +569,10 @@ class BoxSearch(object):
             )
             processes.append(p)
             p.start()
+            episode.internal_process_id += 1
 
         results = [rval.get() for p in processes]
+        rval.close()
 
         for p in processes:
             p.terminate()
@@ -411,8 +581,29 @@ class BoxSearch(object):
         for p in processes:
             p.join()
 
+        episode.false_boxes = [b for r in results for b in r["false_boxes"]]
+        episode.true_boxes = [b for r in results for b in r["true_boxes"]]
+
+        # episode.read_queues()
+        # episode.close()  # Main process close queues
+
         # for p in processes:
         #     r = rval.get()
         #     print(f"Got {r} iterations from process")
+
+        # true_boxes = []
+        # try:
+        #     while not episode.true_boxes.empty():
+        #         true_boxes.append(episode.true_boxes.get_nowait())
+        # except Empty:
+        #     pass
+        # false_boxes = []
+        # num_false = episode.statistics.num_false.acquire(block=False)
+        # try:
+        #     while num_false > 0 and not episode.false_boxes.empty():
+        #         num_false -= 1
+        #         false_boxes.append(episode.false_boxes.get_nowait())
+        # except Empty:
+        #     pass
 
         return episode
