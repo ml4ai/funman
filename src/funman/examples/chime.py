d346e97d76cfe99c619872292253cb8fed53b081
Daniel Bryce <danbryce410@gmail.com>
2022-10-10 09:15:40 -0500

    incremental and assumptions

--- src/funman/examples/chime.py
+++ src/funman/examples/chime.py
@@ -19,9 +19,9 @@ from pysmt.typing import INT, REAL, BOOL
 
 class CHIME(object):
     def make_chime_variables(num_timepoints):
-        susceptible = [Symbol(f"s_{t}", REAL) for t in range(num_timepoints)]
-        infected = [Symbol(f"i_{t}", REAL) for t in range(num_timepoints)]
-        recovered = [Symbol(f"r_{t}", REAL) for t in range(num_timepoints)]
+        susceptible = [Symbol(f"s_{t}", REAL) for t in range(num_timepoints + 1)]
+        infected = [Symbol(f"i_{t}", REAL) for t in range(num_timepoints + 1)]
+        recovered = [Symbol(f"r_{t}", REAL) for t in range(num_timepoints + 1)]
 
         susceptible_n = [Symbol(f"s_n_{t}", REAL) for t in range(num_timepoints)]
         infected_n = [Symbol(f"i_n_{t}", REAL) for t in range(num_timepoints)]
@@ -80,71 +80,63 @@ class CHIME(object):
             ]
         )
 
-        dynamics = And(
-            [
-                And(
-                    [
-                        # r_n = gamma * i + r  # Update to the amount of individuals that are recovered ## sir_r_n_exp
-                        Equals(recovered_n[t + 1], gamma * infected[t] + recovered[t]),
-                        # s_n = (-beta * s * i) + s  # Update to the amount of individuals that are susceptible ## sir_s_n_exp
-                        Equals(
-                            susceptible_n[t + 1],
-                            (-beta * susceptible[t] * infected[t]) + susceptible[t],
-                        ),
-                        # i_n = (beta * s * i - gamma * i) + i  # Update to the amount of individuals that are infectious ## sir_i_n_exp
-                        Equals(
-                            infected_n[t + 1],
-                            (beta * susceptible[t] * infected[t] - gamma * infected[t])
-                            + infected[t],
-                        ),
-                        # scale = n / (s_n + i_n + r_n)  # A scaling factor to compute updated disease variables ## sir_scale_exp
-                        Equals(
-                            scale[t + 1],
-                            n
-                            / (
-                                susceptible_n[t + 1]
-                                + infected_n[t + 1]
-                                + recovered_n[t + 1]
-                            ),
-                        ),
-                        # s = s_n * scale  ## sir_s_exp
-                        Equals(susceptible[t + 1], susceptible_n[t + 1] * scale[t + 1]),
-                        # i = i_n * scale  ## sir_i_exp
-                        Equals(infected[t + 1], infected_n[t + 1] * scale[t + 1]),
-                        # r = r_n * scale  ## sir_r_exp
-                        Equals(recovered[t + 1], recovered_n[t + 1] * scale[t + 1]),
-                    ]
-                )
-                for t in range(num_timepoints - 1)
-            ]
-        )
+        dynamics = [
+            And(
+                [
+                    # r_n = gamma * i + r  # Update to the amount of individuals that are recovered ## sir_r_n_exp
+                    Equals(recovered_n[t], gamma * infected[t] + recovered[t]),
+                    # s_n = (-beta * s * i) + s  # Update to the amount of individuals that are susceptible ## sir_s_n_exp
+                    Equals(
+                        susceptible_n[t],
+                        (-beta * susceptible[t] * infected[t]) + susceptible[t],
+                    ),
+                    # i_n = (beta * s * i - gamma * i) + i  # Update to the amount of individuals that are infectious ## sir_i_n_exp
+                    Equals(
+                        infected_n[t],
+                        (beta * susceptible[t] * infected[t] - gamma * infected[t])
+                        + infected[t],
+                    ),
+                    # scale = n / (s_n + i_n + r_n)  # A scaling factor to compute updated disease variables ## sir_scale_exp
+                    Equals(
+                        scale[t],
+                        n / (susceptible_n[t] + infected_n[t] + recovered_n[t]),
+                    ),
+                    # s = s_n * scale  ## sir_s_exp
+                    Equals(susceptible[t + 1], susceptible_n[t] * scale[t]),
+                    # i = i_n * scale  ## sir_i_exp
+                    Equals(infected[t + 1], infected_n[t] * scale[t]),
+                    # r = r_n * scale  ## sir_r_exp
+                    Equals(recovered[t + 1], recovered_n[t] * scale[t]),
+                ]
+            )
+            for t in range(num_timepoints)
+        ]
 
-        bounds = And(
-            [
-                And(
-                    [
-                        LE(recovered_n[t], n),
-                        GE(recovered_n[t], Real(0.0)),
-                        LE(susceptible_n[t], n),
-                        GE(susceptible_n[t], Real(0.0)),
-                        LE(infected_n[t], n),
-                        GE(infected_n[t], Real(0.0)),
-                        LE(recovered[t], n),
-                        GE(recovered[t], Real(0.0)),
-                        LE(susceptible[t], n),
-                        GE(susceptible[t], Real(0.0)),
-                        LE(infected[t], n),
-                        GE(infected[t], Real(0.0)),
-                    ]
-                )
-                for t in range(num_timepoints)
-            ]
-        )
+        bounds = [
+            And(
+                [
+                    LE(recovered_n[t], n),
+                    GE(recovered_n[t], Real(0.0)),
+                    LE(susceptible_n[t], n),
+                    GE(susceptible_n[t], Real(0.0)),
+                    LE(infected_n[t], n),
+                    GE(infected_n[t], Real(0.0)),
+                    LE(recovered[t + 1], n),
+                    GE(recovered[t + 1], Real(0.0)),
+                    LE(susceptible[t + 1], n),
+                    GE(susceptible[t + 1], Real(0.0)),
+                    LE(infected[t + 1], n),
+                    GE(infected[t + 1], Real(0.0)),
+                ]
+            )
+            for t in range(num_timepoints)
+        ]
+
         return parameters, init, dynamics, bounds
 
     def make_chime_query(infected, num_timepoints):
         threshold = 100
 
         # I_t <= 100
-        query = And([LT(infected[t], Real(threshold)) for t in range(num_timepoints)])
+        query = [LT(infected[t], Real(threshold)) for t in range(num_timepoints)]
         return query
---
+++ test/CHIME/test_chime_incremental.py
@@ -0,0 +1,198 @@
+from cmath import inf
+from contextlib import contextmanager
+from dis import dis
+from pysmt.shortcuts import (
+    get_model,
+    And,
+    Solver,
+    TRUE,
+    reset_env as pysmt_reset_env,
+    is_sat,
+    Symbol,
+    BOOL,
+    Implies,
+    Not,
+    Equals,
+)
+from pysmt.typing import INT, REAL, BOOL
+from pysmt.logics import QF_NRA, QF_LRA, QF_UFLIRA, QF_UFNRA
+
+
+import unittest
+import os
+from funman.examples.chime import CHIME
+from timeit import default_timer
+
+RESOURCES = os.path.join("resources")
+
+
+@contextmanager
+def elapsed_timer():
+    start = default_timer()
+    elapser = lambda: default_timer() - start
+    try:
+        yield elapser
+    finally:
+        elapser = None
+
+
+def reset_env():
+    env = pysmt_reset_env()
+    env.enable_infix_notation = True
+    return env
+
+
+class TestHandcoded(unittest.TestCase):
+    def encode_time_horizon(self, parameters, init, dynamics, horizon):
+        dynamics_t = (
+            And([d_t for d_t in dynamics[0:horizon]]) if horizon > 0 else TRUE()
+        )
+        return And(parameters, init, dynamics_t)
+
+    def run_assumption_solver(
+        self, formulas, solver_name=None, logic=None, solver_options=None
+    ):
+        # with steps
+        with elapsed_timer() as elapsed:
+            with Solver(
+                name=solver_name, logic=logic, solver_options=solver_options
+            ) as solver:
+                assumptions = [Symbol(f"time_{t}", BOOL) for t in range(len(formulas))]
+                assumption_steps = [
+                    Implies(assumption, formula)
+                    for (assumption, formula) in zip(assumptions, formulas)
+                ]
+
+                # solver.add_assertion(phi)
+
+                for step in range(len(assumptions)):
+                    print(f"Step: {step}/{len(assumptions)-1}")
+                    solver.add_assertion(assumption_steps[step])
+                    step_assumptions = [Not(a) for a in assumptions[:step]] + [
+                        assumptions[step]
+                    ]
+                    # enabled_steps = [a for a in assumptions[: step + 1]]
+                    # disabled_steps = [Not(a) for a in assumptions[step + 1 :]]
+                    # step_assumptions = enabled_steps  # + disabled_steps
+                    if not solver.solve(assumptions=step_assumptions):
+                        raise Exception("unsat")
+                    model = solver.get_model()
+                    # solver.add_assertion(assumptions[step])
+                    model_assertions = [
+                        (
+                            Equals(v, model[v])
+                            if not model[v].is_bool_constant()
+                            else None
+                            # else Not(
+                            #     v
+                            # )  # Negate the step assertion, which is the only bool (could be risky)
+                        )
+                        for v in assumption_steps[step].get_free_variables()
+                    ]
+                    for a in model_assertions:
+                        if a:
+                            solver.add_assertion(a)
+                    # print(elapsed())
+
+                # if not solver.solve():
+                #     raise Exception("unsat")
+                # model = solver.get_model()
+                model = None
+                elapsed = elapsed()
+                solver.exit()
+        return model, elapsed
+
+    def run_incremental_solver(
+        self, formulas, solver_name=None, logic=None, solver_options=None
+    ):
+        # with steps
+        with elapsed_timer() as elapsed:
+            with Solver(
+                name=solver_name, logic=logic, solver_options=solver_options
+            ) as solver:
+                for i, phi in enumerate(formulas):
+                    if i > 0:
+                        solver.push()
+                    solver.add_assertion(phi)
+
+                    # if not solver.solve():
+                    #     raise Exception("unsat")
+
+                if not solver.solve():
+                    raise Exception("unsat")
+                # model = solver.get_model()
+                model = None
+                elapsed = elapsed()
+                solver.exit()
+        return model, elapsed
+
+    def run_get_model(
+        self, formulas, solver_name=None, logic=None, solver_options=None
+    ):
+        # just model
+        with elapsed_timer() as elapsed:
+            model = get_model(
+                And(formulas),
+                solver_name=solver_name,
+                logic=logic,
+                # solver_options=solver_options,
+            )
+            elapsed = elapsed()
+        return model, elapsed
+
+    def test_simple_chime_propositional(self):
+
+        min_num_timepoints = 1
+        max_num_timepoints = 15
+
+        solver_options = {
+            "nlsat.check_lemmas": True,
+            # "dot_proof_file": "z3_proof.dot"
+            # "add_bound_upper": 1010,
+            # "add_bound_lower": 0,
+            # "propagate_values.max_rounds": 10
+            # "arith.nl.nra": True,
+            # "arith.nl.grobner": False,
+            # "arith.nl.horner": False,
+            # "arith.nl.order": False,
+            # "arith.nl.tangents": False,
+            # "threads": 2
+        }
+
+        # query = CHIME.make_chime_query(infected, num_timepoints)
+        print(f"steps\torig\tinc")
+        for num_timepoints in range(min_num_timepoints, max_num_timepoints):
+            vars = CHIME.make_chime_variables(num_timepoints)
+            parameters, init, dynamics, bounds = CHIME.make_chime_model(
+                *vars,
+                num_timepoints,
+            )
+            phi = self.encode_time_horizon(parameters, init, dynamics, num_timepoints)
+            phi_stratified = [And(init, parameters), *dynamics[0:num_timepoints]]
+            reset_env()
+            model, elapsed = self.run_get_model(
+                phi, solver_name="z3", logic=QF_UFLIRA, solver_options=solver_options
+            )
+            reset_env()
+            asm_model, asm_elapsed = self.run_assumption_solver(
+                phi_stratified,
+                solver_name="z3",
+                logic=QF_UFLIRA,
+                solver_options=solver_options,
+            )
+
+            # reset_env()
+            # inc_model, inc_elapsed = self.run_incremental_solver(
+            #     phi_stratified,
+            #     solver_name="z3",
+            #     logic=QF_UFLIRA,
+            #     solver_options=solver_options,
+            # )
+            inc_elapsed = 0
+            print(
+                f"{num_timepoints}\t{float('{:.2f}'.format(elapsed))}\t{float('{:.2f}'.format(inc_elapsed))}\t{float('{:.2f}'.format(asm_elapsed))}"
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
