103ea34d1d94325617398087412a5635c9523113
Drisana Mosaphir <dmosaphir@sift.net>
2022-10-12 14:34:48 -0500

    updates to intersection and symmetric difference. corresponding changes will also be made in search_utils.py

--- src/funman/parameter_space.py
+++ src/funman/parameter_space.py
@@ -2,7 +2,8 @@ from typing import List
 from funman.plotting import BoxPlotter
 from funman.search_episode import SearchEpisode
 from funman.search_utils import Box
-
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
 class ParameterSpace(object):
     def __init__(self, true_boxes: List[Box], false_boxes: List[Box]) -> None:
@@ -15,56 +16,24 @@ class ParameterSpace(object):
         raise NotImplementedError()
         return ParameterSpace()
 
-#    def intersect(ps1, ps2) -> "ParameterSpace":
-#        # FIXME Drisana
-#        raise NotImplementedError()
-#        return ParameterSpace()
-
-    # STUB intersect parameters spaces
+#    # STUB intersect parameters spaces
     @staticmethod
-    def intersect(ps1, ps2) -> "ParameterSpace":
+    def intersect(ps1, ps2):
         results_list = []
-        for box1 in ps1.bounds.values():
-            for box2 in ps2.bounds.values():
-                subresult = intersect_two_boxes(box1, box2)
+        for box1 in ps1:
+            for box2 in ps2:
+                subresult = Box.intersect_two_boxes(box1, box2)
                 if subresult != None:
                     results_list.append(subresult)
         return results_list
 
     @staticmethod
-    def intersect_two_1d_boxes(a,b):
-        """Given 2 intervals a = [a0,a1] and b=[b0,b1], check whether they intersect.  If they do, return their intersection."""
-        if a[0] <= b[0]:
-            minArray = a
-            maxArray = b
-        else:
-            minArray = b
-            maxArray = a
-        if minArray[1] > maxArray[0]: ## has nonempty intersection. return intersection
-            return [maxArray[0], minArray[1]]
-        else: ## no intersection.
-            return []
-
-    @staticmethod
-    def intersect_two_boxes(a,b):
-        result = []
-        d = len(a) ## dimension
-        for i in range(d):
-            subresult = intersect_two_1d_boxes(a[i],b[i])
-            if subresult == []:
-                return None
-            else:
-                result.append(subresult)
-        return result
-
-    # STUB symmetric_difference of parameter spaces
-    @staticmethod
-    def symmetric_difference(ps1, ps2) -> "ParameterSpace":
+    def symmetric_difference(ps1, ps2):
         results_list = []
         for box2 in ps2:
             box2_results = []
             for box1 in ps1:
-                subresult = symmetric_difference_two_boxes(box2, box1)
+                subresult = Box.symmetric_difference_two_boxes(box2, box1)
                 if subresult != None:
                     box2_results.append(subresult[0])
                 elif subresult == None:
@@ -77,7 +46,7 @@ class ParameterSpace(object):
         for box1 in ps1:
             box1_results = []
             for box2 in ps2:
-                subresult = symmetric_difference_two_boxes(box1, box2)
+                subresult = Box.symmetric_difference_two_boxes(box1, box2)
                 if subresult != None:
                     box1_results.append(subresult[0])
                 elif subresult == None:
@@ -87,57 +56,25 @@ class ParameterSpace(object):
             else:
                 results_list.append(box1_results[0])
         return results_list
-
-    @staticmethod
-    def symmetric_difference_two_1d_boxes(a,b):
-        """Given 2 intervals a = [a0,a1] and b=[b0,b1], return their symmetric difference (points not in their intersection)."""
-        if a == b: ## no symmetric difference
-            return None
-        else:
-            if a[0] <= b[0]:
-                minArray = a
-                maxArray = b
-            else:
-                minArray = b
-                maxArray = a
-            if minArray[1] > maxArray[0]: ## has nonempty intersection. form symmetric differences and return them
-                return [[minArray[0], maxArray[0]],[minArray[1], maxArray[1]]]
-            else: ## no intersection. symmetric difference is the entirety of the 2 arrays
-                return [minArray, maxArray]
-
-    @staticmethod
-    def subtract_two_1d_boxes(a,b):
-        """Given 2 intervals a = [a0,a1] and b=[b0,b1], return the part of a that does not intersect with b."""
-        if intersect_two_1d_boxes(a,b) == None:
-            return a
-        else:
-            if a[0] < b[0]:
-                return [a[0],b[0]]
-            elif a[0] > b[0]:
-                return [b[1],a[1]]
-    
-    @staticmethod
-    def symmetric_difference_two_boxes(a,b): ### WIP - just for 2 dimensions at this point.
-        result = []
-        if a == b:
-            result = None 
-        elif intersect_two_boxes(a,b) == None: ## no intersection so they are disjoint - return both original boxes
-            result = [a,b]
-        else:
-            xbounds = subtract_two_1d_boxes(a[0],b[0])
-            if xbounds != None:
-                result.append([xbounds,a[1]])
-            xbounds = subtract_two_1d_boxes(b[0],a[0])
-            if xbounds != None:
-                result.append([xbounds,b[1]])
-            ybounds = subtract_two_1d_boxes(a[1],b[1])
-            if ybounds != None:
-                result.append([a[0],ybounds]) 
-            ybounds = subtract_two_1d_boxes(b[1],a[1])
-            if ybounds != None:
-                result.append([b[0],ybounds])         
-        return result
-            
+#
+#    @staticmethod
+#    def symmetric_difference_two_1d_boxes(a,b):
+#        """Given 2 intervals a = [a0,a1] and b=[b0,b1], return their symmetric difference (points not in their intersection)."""
+#        if a == b: ## no symmetric difference
+#            return None
+#        else:
+#            if a[0] <= b[0]:
+#                minArray = a
+#                maxArray = b
+#            else:
+#                minArray = b
+#                maxArray = a
+#            if minArray[1] > maxArray[0]: ## has nonempty intersection. form symmetric differences and return them
+#                return [[minArray[0], maxArray[0]],[minArray[1], maxArray[1]]]
+#            else: ## no intersection. symmetric difference is the entirety of the 2 arrays
+#                return [minArray, maxArray]
+#
+#            
     
     # STUB construct space where all parameters are equal
     @staticmethod
@@ -151,8 +88,14 @@ class ParameterSpace(object):
         raise NotImplementedError()
         raise NotImplementedError()
 
-    def plot():
-        # FIXME Drisana
-        box_plotter = BoxPlotter([], [])
-        box_plotter.plot_list_of_boxes()
+    @staticmethod
+    def plot(ps1, color="b",alpha=0.2):
+        custom_lines = [
+            Line2D([0], [0], color=color, lw=4,alpha=alpha),
+        ]
+        
+        plt.legend(custom_lines, ["ps1"])
+        for b1 in ps1:
+            BoxPlotter.plot2DBoxList(b1, color='b')         
+        plt.show() 
         pass
