40abfdc405fe33804de34b1a8aa6ab8310c76457
Jack Ladwig <jladwig@sift.net>
2022-12-01 17:37:20 -0600

    add missing deps and mark as 1.2.1

--- setup.py
+++ setup.py
@@ -21,7 +21,11 @@ setup(name='funman',
       packages=find_packages('src'),
       package_dir={'':'src'},
       install_requires=[
-        "model2smtlib"
+        "funman_dreal",
+        "graphviz",
+        "model2smtlib",
+        "multiprocess",
+        "tenacity"
         #"automates @ https://github.com/danbryce/automates/archive/e5fb635757aa57007615a75371f55dd4a24851e0.zip#sha1=f9b3c8a7d7fa28864952ccdd3293d02894614e3f"
       ],
       tests_require=["unittest"],
