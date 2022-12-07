67992f49423ce0ad54e2d7cb350ecb0f5b4f7e1d
Drisana Mosaphir <dmosaphir@sift.net>
2022-12-07 11:39:28 -0600

    add Scenario 2: parameter synthesis of vaccination rate

--- auxiliary_packages/funman_demo/src/funman_demo/example/demo120822.py
+++ auxiliary_packages/funman_demo/src/funman_demo/example/demo120822.py
@@ -374,3 +374,246 @@ SIR Bilayer (left), Hospitalized Measurement (right)
 
     def analyze_intervention_3(self, transmission_reduction):
         pass
+
+
+class Scenario2(object):
+    def __init__(
+        self,
+        init_values={"S": 9996, "V": 1, "I": 1, "I_v":1, "R": 1},
+        query_threshold=10000,
+        duration=1,
+        step_size=1,
+    ):
+        # Define the dynamics with a bilayer
+
+        self.chime_sviivr_bilayer_src = {
+            "Qin": [
+                {"variable": "S"},
+                {"variable": "V"},
+                {"variable": "I"},
+                {"variable": "I_v"},
+                {"variable": "R"},
+            ],
+            "Box": [
+                {"parameter": "beta_1"},
+                {"parameter": "beta_2"},
+                {"parameter": "v_r"},
+                {"parameter": "v_s1"},
+                {"parameter": "v_s2"},
+                {"parameter": "gamma_1"},
+                {"parameter": "gamma_2"},
+            ],
+            "Qout": [
+                {"tanvar": "S'"},
+                {"tanvar": "V'"},
+                {"tanvar": "I'"},
+                {"tanvar": "I_v'"},
+                {"tanvar": "R'"},
+            ],
+            "Win": [
+                {"arg": 1, "call": 1},
+                {"arg": 1, "call": 2},
+                {"arg": 1, "call": 3},
+                {"arg": 2, "call": 4},
+                {"arg": 2, "call": 5},
+                {"arg": 3, "call": 1},
+                {"arg": 3, "call": 4},
+                {"arg": 3, "call": 6},
+                {"arg": 4, "call": 2},
+                {"arg": 4, "call": 5},
+                {"arg": 4, "call": 7},
+            ],
+            "Wa": [
+                {"influx": 1, "infusion": 3},
+                {"influx": 2, "infusion": 3},
+                {"influx": 3, "infusion": 2},
+                {"influx": 4, "infusion": 4},
+                {"influx": 5, "infusion": 4},
+                {"influx": 6, "infusion": 5},
+                {"influx": 7, "infusion": 5},
+            ],
+            "Wn": [
+                {"efflux": 1, "effusion": 1},
+                {"efflux": 2, "effusion": 1},
+                {"efflux": 3, "effusion": 1},
+                {"efflux": 4, "effusion": 2},
+                {"efflux": 5, "effusion": 2},
+                {"efflux": 6, "effusion": 3},
+                {"efflux": 7, "effusion": 4},
+            ],
+        }
+
+        self.chime_sviivr_bilayer = Bilayer.from_json(
+            self.chime_sviivr_bilayer_src
+        )
+
+        # Define the measurements made of the bilayer variables
+        # Hospitalizations (H) are a proportion (hr) of those infected (I)
+
+        self.measurements = {
+            "state": [{"variable": "I"}],
+            "observable": [{"observable": "H"}],
+            "rate": [{"parameter": "hr"}],
+            "Din": [{"variable": 1, "parameter": 1}],
+            "Dout": [{"parameter": 1, "observable": 1}],
+        }
+        self.hospital_measurements = BilayerMeasurement.from_json(
+            self.measurements
+        )
+
+        # Model Setup for both Intervention 1
+        # - Prescribed reduction in transmission, i.e., beta' = (1-transmission_reduction)beta
+
+        self.config = {
+            "vaccination_increase": 0.05,
+            "duration": duration,  # 10
+            "step_size": step_size,
+            "query_variable": "H",
+            "query_threshold": query_threshold,
+        }
+
+        self.models = {
+            "intervention_vaccination": {
+                "SVIIR": BilayerModel(
+                    self.chime_sviivr_bilayer,
+                    measurements=self.hospital_measurements,
+                    init_values={"S": 10000, "V": 1, "I": 1, "I_v": 1, "R": 1},
+                    parameter_bounds={
+                        "beta_1": [0.000067, 0.000067],
+                        "beta_2": [0.000067, 0.000067],
+                        "gamma_1": [1.0 / 14.0, 1.0 / 14.0],
+                        "gamma_2": [1.0 / 14.0, 1.0 / 14.0],
+                        "v_s1": [0.000067, 0.000067],
+                        "v_s2": [0.000067, 0.000067],
+                        "v_r": [0.05, 0.05],
+                        "hr": [1.0, 1.0],
+                    },
+                ),
+            },
+        }
+
+        self.encoding_options = BilayerEncodingOptions(
+            step_size=self.config["step_size"],
+            max_steps=self.config["duration"],
+        )
+
+        # query = QueryTrue()
+        self.query = QueryLE(
+            self.config["query_variable"], self.config["query_threshold"]
+        )
+
+    def to_md(self, model):
+
+        model.measurements.to_dot().render(filename="measurement", format="png")
+        model.bilayer.to_dot().render(filename="bilayer", format="png")
+
+        init_values_md = "\n".join(
+            {f"- ## {k}: {v}" for k, v in model.init_values.items()}
+        )
+        parameters_bounds_md = "\n".join(
+            {f"- ## {k}: {v}" for k, v in model.parameter_bounds.items()}
+        )
+        q = (
+            "\\bigwedge\\limits_{t \\in [0,"
+            + str(self.config["duration"])
+            + "]} "
+            + self.config["query_variable"]
+            + "_t \\leq"
+            + str(self.config["query_threshold"])
+        )
+        config_md = "\n".join(
+            {f"- ## {k}: {v}" for k, v in self.config.items()}
+        )
+        self.md = md(
+            f"""# Bilayer and Measurement Model
+SIR Bilayer (left), Infected Measurement (right)
+
+![](bilayer.png) ![](measurement.png)
+# Initial State (population 10000)
+{init_values_md}
+# Parameter Bounds
+{parameters_bounds_md}
+# Scenario Configuration
+{config_md}
+# Query
+- ## ${q}$
+"""
+        )
+        return self.md
+
+#    def compare_model_results(self, results):
+#        df = pd.DataFrame(
+#            {
+#                f"{name}": result["dataframe"]["H"]
+#                for name, result in results.items()
+#                if result["dataframe"] is not None
+#            }
+#        )
+#        df = df.apply(lambda x: self.config["query_threshold"] - x)
+#        if len(df) > 0:
+#            ax = df.boxplot()
+#            ax.set_title("Unused Hospital Capacity per Day")
+#            ax.set_xlabel("Model")
+#            ax.set_ylabel("Unused Hospital Capacity")
+
+    def analyze_intervention_vaccination(self, vaccination_increase):
+        if not isinstance(vaccination_increase, list):
+            raise Exception(
+                f"vaccination_increase must be a list of the form [lb, ub]"
+            )
+
+        results = []
+        for model in self.models["intervention_vaccination"]:
+
+            lb = model.parameter_bounds["v_r"][0] * (
+                1.0 + vaccination_increase[0]
+            )
+            ub = model.parameter_bounds["v_r"][1] * (
+                1.0 + vaccination_increase[1]
+            )
+            model.parameter_bounds["v_r"] = [lb, ub]
+
+            parameters = [Parameter("v_r", lb=lb, ub=ub)]
+            tmp_dir_path = tempfile.mkdtemp(prefix="funman-")
+            result = Funman().solve(
+                ParameterSynthesisScenario(
+                    parameters,
+                    model,
+                    self.query,
+                    smt_encoder=BilayerEncoder(config=self.encoding_options),
+                ),
+                config=SearchConfig(
+                    number_of_processes=1,
+                    tolerance=1e-8,
+                    solver="dreal",
+                    search=BoxSearch,
+                    # wait_action = NotebookImageRefresher(os.path.join(tmp_dir_path, "search.png"), sleep_for=1),
+                    handler=ResultCombinedHandler(
+                        [
+                            ResultCacheWriter(
+                                os.path.join(tmp_dir_path, "search.json")
+                            ),
+                            RealtimeResultPlotter(
+                                parameters,
+                                plot_points=True,
+                                realtime_save_path=os.path.join(
+                                    tmp_dir_path, "search.png"
+                                ),
+                            ),
+                        ]
+                    ),
+                ),
+            )
+            msg = ""
+            plot = result.plot()
+            df = pd.DataFrame()
+            results.append(
+                {
+                    "message": msg,
+                    "plot": plot,
+                    "dataframe": df,
+                    "parameter_space": result.parameter_space,
+                }
+            )
+        return results
+
