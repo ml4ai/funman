826bfc9cbdf9cedaee8c3df579a6f464c6707609
Daniel Bryce <danbryce410@gmail.com>
2023-05-19 14:56:36 +0000

    ensemble for petri nets working

--- src/funman/model/ensemble.py
+++ src/funman/model/ensemble.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Dict, List, Tuple
 
 import graphviz
 from pydantic import BaseModel
@@ -11,6 +11,18 @@ from .model import Model
 
 class EnsembleModel(Model):
     models: List[Model]
+    _model_name_map: Dict[str, Model] = None
+    _var_name_map: Dict[str, Tuple[str, Model]] = None
+    _parameter_name_map: Dict[str, Tuple[str, Model]] = None
+    _parameter_map: Dict[str, Parameter] = None
+
+    class Config:
+        underscore_attrs_are_private = True
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.models = kwargs["models"]
+        self._initialize_mappings()
 
     def default_encoder(self, config: "FUNMANConfig") -> "Encoder":
         """
@@ -27,22 +39,47 @@ class EnsembleModel(Model):
         )
 
     def _get_init_value(self, var: str):
-        return self.init_values[var]
+        (m_name, orig_var) = self._var_name_map[var]
+        return self._model_name_map[m_name].init_values[orig_var]
 
     def _state_vars(self):
         return map(lambda m: m._state_vars(), self.models)
 
+    def _initialize_mappings(self):
+        self._model_name_map = {m.name: m for m in self.models}
+        model_vars = {m.name: m._state_var_names() for m in self.models}
+        self._var_name_map = {
+            f"model_{m_name}_{v}": (m_name, v)
+            for m_name, _ in self._model_name_map.items()
+            for v in model_vars[m_name]
+        }
+        model_parameters = {m.name: m._parameter_names() for m in self.models}
+        self._parameter_name_map = {
+            f"model_{m_name}_{p}": (m_name, p)
+            for m_name, _ in self._model_name_map.items()
+            for p in model_parameters[m_name]
+        }
+        self._parameter_map = {
+            p_name: Parameter(
+                name=p_name,
+                lb=self._model_name_map[m_name].parameter_bounds[p][0],
+                ub=self._model_name_map[m_name].parameter_bounds[p][1],
+            )
+            for p_name, (m_name, p) in self._parameter_name_map.items()
+            if p in self._model_name_map[m_name].parameter_bounds
+        }
+
     def _state_var_names(self):
-        return map(lambda m: m._state_var_names(), self.models)
+        return list(self._var_name_map.keys())
 
     def _parameter_names(self):
-        return map(lambda m: m._parameter_names(), self.models)
+        return list(self._parameter_name_map.keys())
 
     def _parameter_values(self):
         return map(lambda m: m._parameter_values(), self.models)
 
     def _parameters(self) -> List[Parameter]:
-        return map(lambda m: m._parameters(), self.models)
+        return list(self._parameter_map.values())
 
     def to_dot(self, values={}):
         """
